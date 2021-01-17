3600e077b137812706e17dbf76166dca72901128
w <308932645@qq.com>
2021-01-17 10:22:56 +0800

    init

---
+++ RentalPlatform/rent-provider-manager-8002/src/main/java/com/rent/ManagerProvider_8001.java
@@ -0,0 +1,16 @@
+package com.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author w
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+public class ManagerProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerProvider_8001.class,args);
+    }
+}
---
+++ RentalPlatform/rent-provider-user-8001/src/main/java/com/rent/UserProvider_8001.java
@@ -0,0 +1,16 @@
+package com.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author w
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+public class UserProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(UserProvider_8001.class,args);
+    }
+}
